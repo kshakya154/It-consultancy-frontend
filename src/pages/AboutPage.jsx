9ffaec4311008e5da0e838948e66572f6ed39e99
Kamal Kumar <kshakya154@gmail.com>
2026-02-22 00:17:40 +0530

    Fixed image path case issue

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import AboutHero from "../components/About Page Components/AboutHero";
-import AboutMission from "../components/About Page Components/AboutMission";
-import AboutWhatWeDo from "../components/About Page Components/AboutWhatWeDo";
-import AboutTechnologyIndex from "../components/About Page Components/AboutTechnologyIndex";
+import AboutHero from "../components/About-Page-Components/AboutHero";
+import AboutMission from "../components/About-Page-Components/AboutMission";
+import AboutWhatWeDo from "../components/About-Page-Components/AboutWhatWeDo";
+import AboutTechnologyIndex from "../components/About-Page-Components/AboutTechnologyIndex";
 function AboutPage() {
   return (
     <div>
